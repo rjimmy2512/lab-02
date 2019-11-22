6fdfd49d6b7db20ef6fbcec90e5351f7d04625c1
R.James <rjimmy2512@gmail.com>
2019-11-21 22:42:04 -0800

    retouch for resub

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-undef */
 'use strict';
 
 function Picture(horn) {
@@ -28,7 +27,6 @@ Picture.prototype.render = function() {
 };
 
 //Use AJAX, specifically $.get(), to read the provided JSON file.
-//Failed to load resource: the server responded with a status of 404 (Not Found)
 
 Picture.readJson = () => {
   $.get('./data/page-1.json')
@@ -42,11 +40,83 @@ Picture.readJson = () => {
 
 Picture.loadHorns = () => {
   Picture.allHorns.forEach(horn => horn.render());
+
+  Picture.populateFilter();
 };
 
 $(() => Picture.readJson());
 
 
+// Filter function to view image corresponding to a picked keyword
+
+Picture.populateFilter = () => {
+  let filterKeywords = [];
+
+  $('option').not(':first').remove();
+
+  Picture.allHorns.forEach(horn => {
+    if(!filterKeywords.includes(horn.keyword)) {
+      filterKeywords.push(horn.keyword);
+    }
+  });
 
 
+  filterKeywords.sort();
+  filterKeywords.forEach(keyword => {
+    let optionTag = `<option value="${keyword}">${keyword}</option>`;
+    $('select').append(optionTag);
+  });
+};
+
+
+Picture.handleFilter = () => {
+  $('select').on('change', function() {
+    let selected = $(this).val();
+    if(selected !== 'default') {
+      $('div').hide();
+      $(`div.${selected}`).fadeIn();
+    }
+  });
+};
+
+$(() => Picture.handleFilter());
+
+
+// function to sort the order in which images are rendered
+
+Picture.handleSort = () => {
+  $('input').on('change', function() {
+    $('select').val('default');
+    $('div').remove();
+    Picture.sortBy(Picture.allHorns, $(this).attr('id'));
+    Picture.allHorns.forEach(horn => {
+      $('#image-container').append(horn.render());
+    });
+  });
+};
+
+$(() => Picture.handleSort());
+
+
+// function to display a clicked-on image
+
+Picture.handleImageEvents = () => {
+  $('main').on('click', 'div', function(event) {
+    event.stopPropagation();
+    let $clone = $(this).clone();
+    let elements = $clone[0].children;
+
+    $('section').addClass('active').html(elements);
+
+    $(window).scrollTop(0);
+  });
+
+  $('body').on('click', function() {
+    const $section = $('section');
+    $section.empty();
+    $section.removeClass('active');
+  });
+};
+
+$(() => Picture.handleImageEvents());
 
